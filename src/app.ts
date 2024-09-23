57ad001a5e48847ad386c577d659be644324f250
aim <ahmadhakim404@gmail.com>
2024-09-23 18:31:17 +0700

    init rest api + error handler with dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
 import { Hono } from 'hono';
 
-const app = new Hono();
-app.get('/', (c) => c.text('Hello Bun!'));
+// Create a new Hono app with the base path set to /api
+const app = new Hono().basePath('/api');
 
+app.get('/', (c) => c.json({ hello: 'bun' }));
+
+// Add a 404 handler
+app.notFound((c) => {
+	c.status(404);
+	return c.json({
+		errors: true,
+		message: 'Endpoint not found',
+	});
+});
+
+// Add a default error handler
+app.onError((err, c) => {
+	console.error(`${err}`);
+	c.status = c.status || 500;
+	err.message = err.message || 'Internal Server Error';
+	return c.json({
+		errors: true,
+		message: err.message,
+	});
+});
+
+// Set the port to 3000
 export default {
-	port: 3000,
+	port: process.env.PORT || 3000,
 	fetch: app.fetch,
 };
